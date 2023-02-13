d5bba33fe7efab5f634cf8713c1d227464b6cea2
Krzysztof Rzeczyca <krzeczyca@interia.pl>
2023-02-13 16:33:15 +0100

    first

---
+++ bck.cmd
@@ -0,0 +1,11 @@
+FOR /F "tokens=1-3 delims= " %%A IN ('dir "C:\tmp\log\*.log" /b /o-d') DO (
+  IF %count% LEQ 2 (
+    set "file=%%A %%B %%C"
+    set /a count+=1
+	set count
+  ) ELSE  (
+    set "file=%%A %%B %%C"
+	move "C:\tmp\log\%file%" "C:\tmp\log2"
+  )
+)
+set count=0
