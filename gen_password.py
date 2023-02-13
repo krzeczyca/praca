b99fa60913c49a2557c1916007902e840749a98f
Krzysztof Rzeczyca <krzeczyca@interia.pl>
2023-02-13 21:22:12 +0100

    add save to file

--- gen_password.py
+++ gen_password.py
@@ -7,9 +7,26 @@ Created on Thu Jan 26 07:50:25 2023
 import string
 import random
 
+def input_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print ("This is not a number!")
+
+
 def generate_password(length=8, chars=string.ascii_letters + string.digits + string.punctuation):
     return ''.join(random.choice(chars) for i in range(length))
 
 
 if __name__ =="__main__":
-    print(generate_password(15))
+    file = input("What file name? ")
+    much = input_int("How many password to generate? ")
+    long = input_int("How long do you want the string to be?  ")  
+    with open(file, 'x', encoding="utf-8") as f:
+        for i in range(much):
+            f.write(str(i+1)+". "+generate_password(long)+"\n")
+    print("The results have been saved to a file",file)
+    
+    
+    #print(generate_password(15))
